1d5375de0f4849c6924c410eb8cc3b749fe38ce7
timlin13 <tim_lin@becreator.com.tw>
2021-09-29 09:05:39 +0800

    ///

--- src/ChartData.jsx
+++ src/ChartData.jsx
@@ -28,7 +28,10 @@ const intervals = [
     label: "3M",
     value: 90,
   },
-  
+  {
+    label: "MAX",
+    value: "max",
+  },
 ];
 
 const useGetChartData = (cryptoName, interval, options) => {
